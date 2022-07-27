6fd835c1e38ec65fb8143eeb363632025cd69d1c
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-07-27 20:30:31 -0300

    Add function Delete

--- app.js
+++ app.js
@@ -63,6 +63,14 @@ const app = express();
 
  })
 
+ app.delete('/delete', async (req,res) =>{
+    try{
+        await Model.findByIdAndDelete(req.body.id);
+        return res.status(200).json({ success: true, msg: 'Product Deleted' })}catch(err){
+            console.log(err)
+        }
+        })
+
  app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
