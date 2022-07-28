064147a3afa67d741a35d737ef3c0c781be56d79
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-07-27 23:40:58 -0300

    updates on Database

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ const app = express();
 
  app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin","*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    
+        res.header("Access-Control-Allow-Headers","*");
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    
     next();
   });
 
