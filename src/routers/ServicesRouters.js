d5d286ae1f2c0765fb21859b4cb2b126885077de
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-08-08 12:46:57 -0300

    Improvements in CORS

--- src/routers/ServicesRouters.js
+++ src/routers/ServicesRouters.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router();
 const cors = require('cors')
 
-app.use(cors({
+express.use(cors({
     
     origin:'*',
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
